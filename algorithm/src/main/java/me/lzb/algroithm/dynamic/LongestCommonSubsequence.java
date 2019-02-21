03a9fb4c7f46987bf2d3c3ef0c85280294389ebf
egan <egan@2dfire.com>
2019-02-21 20:25:36 +0800

    最长公共子序

---
+++ algorithm/src/main/java/me/lzb/algroithm/dynamic/LongestCommonSubsequence.java
@@ -0,0 +1,87 @@
+package me.lzb.algroithm.dynamic;
+
+/**
+ * 最长公共子序
+ * Created by lzb on 19/2/20
+ */
+public class LongestCommonSubsequence {
+
+
+    public static String common(String strA, String strB) {
+        if (strA == null || strB == null) {
+            return null;
+        }
+
+        int al = strA.length();
+        int bl = strB.length();
+
+
+        char[] a = strA.toCharArray();
+        char[] b = strB.toCharArray();
+
+        char[] tmp = new char[0];
+        for (int i = 0; i < al + bl - 1; i++) {
+            //左下角到右上角
+            int l; //纵坐标
+            int r;//横坐标
+            int tmpL;//这次的子串长度
+            if (al > i) {
+                l = al - 1 - i;
+                r = 0;
+                tmpL = al - l;
+            } else {
+                l = 0;
+                r = i - al;
+                tmpL = bl - r;
+            }
+
+            //因为中间可能断掉不连续，所以两个临时数组，while循环结束后选长的；
+            char[] tc1 = new char[tmpL];
+            char[] tc2 = new char[tmpL];
+            int idx1 = 0;//数组当前用到的坐标
+            int idx2 = 0;
+            boolean useTc1 = true;
+            while (l < al && r < bl) {
+                if (a[l] == b[r]) {
+                    if (useTc1) {
+
+                        tc1[idx1] = a[l];
+                        idx1 = idx1 + 1;
+                    } else {
+                        tc2[idx2] = a[l];
+                        idx2 = idx2 + 1;
+                    }
+                } else {
+                    if (idx2 < idx1) {
+                        useTc1 = false;
+                        idx2 = 0;
+                    } else {
+                        useTc1 = true;
+                        idx1 = 0;
+                    }
+                }
+                l = l + 1;
+                r = r + 1;
+            }
+
+            char[] tc = new char[idx2 > idx1 ? idx2 : idx1];
+            if (idx2 > idx1) {
+                System.arraycopy(tc2, 0, tc, 0, idx2);
+            } else {
+                System.arraycopy(tc1, 0, tc, 0, idx1);
+            }
+            if (tc.length > tmp.length) {
+                tmp = tc;
+            }
+
+        }
+        return String.valueOf(tmp);
+    }
+
+    public static void main(String[] args) {
+        String a = "cfabd";
+        String b = "caabc";
+        System.out.println(common(a, b));
+    }
+
+}
--- algorithm/src/main/java/me/lzb/algroithm/leetcode/n5/Solution.java
+++ algorithm/src/main/java/me/lzb/algroithm/leetcode/n5/Solution.java
@@ -67,9 +67,119 @@ public class Solution {
     }
 
 
+    /**
+     * 最长公共子串解法
+     * 居然还是暴力法快。。。
+     *
+     * @param s
+     * @return
+     */
+    public static String longestCommonSubsequence(String s) {
+        if (s == null || "".equals(s)) {
+            return "";
+        }
+
+        int al = s.length();
+        int bl = s.length();
+
+        char[] a = s.toCharArray();
+        //颠倒
+        char[] b = new char[a.length];
+        for (int i = 0; i < a.length; i++) {
+            b[b.length - 1 - i] = a[i];
+        }
+
+        char[] tmp = {a[0]};
+        for (int i = 0; i < al + bl - 1; i++) {
+            //左下角到右上角
+            int l; //纵坐标
+            int r;//横坐标
+            int tmpL;//这次的子串长度
+            if (al > i) {
+                l = al - 1 - i;
+                r = 0;
+                tmpL = al - l;
+            } else {
+                l = 0;
+                r = i - al;
+                tmpL = bl - r;
+            }
+            //如果最长长度都没有当前找到的回文子串长，就跳过这次
+            if (tmpL <= tmp.length) {
+                continue;
+            }
+
+            //因为中间可能断掉不连续，所以两个临时数组，while循环结束后选长的；
+            char[] tc1 = new char[tmpL];
+            char[] tc2 = new char[tmpL];
+            int idx1 = 0;//数组当前用到的坐标
+            int idx2 = 0;
+            boolean useTc1 = true;
+            while (l < al && r < bl) {
+
+                if (a[l] == b[r]) {
+                    if (useTc1) {
+                        tc1[idx1] = a[l];
+                        idx1 = idx1 + 1;
+                    } else {
+                        tc2[idx2] = a[l];
+                        idx2 = idx2 + 1;
+                    }
+                } else {
+                    char[] tt = getCharArray(tmp.length, useTc1, tc1, idx1, tc2, idx2);
+                    if(tt!= null){
+                        tmp = tt;
+                    }
+
+                    if (idx2 < idx1) {
+                        useTc1 = false;
+                        idx2 = 0;
+                    } else {
+                        useTc1 = true;
+                        idx1 = 0;
+                    }
+                }
+                l = l + 1;
+                r = r + 1;
+                char[] tt = getCharArray(tmp.length, useTc1, tc1, idx1, tc2, idx2);
+                if(tt!= null){
+                    tmp = tt;
+                }
+            }
+        }
+
+        return String.valueOf(tmp);
+    }
+
+
+    private static char[] getCharArray(int tmpLength, boolean useTc1, char[] tc1, int idx1, char[] tc2, int idx2) {
+        if (useTc1) {
+            if (idx1 > tmpLength) {
+                if (isPalindrome(tc1, idx1)) {
+                    char[] cc = new char[idx1];
+                    System.arraycopy(tc1, 0, cc, 0, idx1);
+                    return cc;
+                }
+            }
+        } else {
+            if (idx2 > tmpLength) {
+                if (isPalindrome(tc2, idx2)) {
+
+                    char[] cc = new char[idx2];
+                    System.arraycopy(tc2, 0, cc, 0, idx2);
+                    return cc;
+                }
+            }
+        }
+
+        return null;
+
+    }
+
+
     public static void main(String[] args) {
-        String s = "";
-        System.out.println(longestPalindrome(s));
+        String s = "bb";
+        System.out.println(longestCommonSubsequence(s));
     }
 
 }
