9d88e7b378597fd7ffc007074d02f25eb8985277
lzb <lzbferrari@gmail.com>
2019-03-08 01:02:49 +0800

    增加model

---
+++ java-basic/src/main/java/me/lzb/java/basic/threadpopl/TPDemo.java
@@ -0,0 +1,12 @@
+package me.lzb.java.basic.threadpopl;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+/**
+ * @author LZB
+ */
+public class TPDemo {
+
+    private static Executor executor = Executors.newScheduledThreadPool(100);
+}
