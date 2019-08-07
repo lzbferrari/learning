eeb9fa7c3fa7abe281c6cb92dc667e745a422637
egan <egan@2dfire.com>
2019-08-07 18:11:11 +0800

    QAQ

---
+++ java-basic/src/main/java/me/lzb/java/basic/concurrent/ConditionDemo.java
@@ -0,0 +1,51 @@
+package me.lzb.java.basic.concurrent;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by lzb on 19/8/7
+ */
+public class ConditionDemo {
+    final Lock lock = new ReentrantLock();
+    // condition 依赖于 lock 来产生
+    final Condition notFull = lock.newCondition();
+    final Condition notEmpty = lock.newCondition();
+
+    final Object[] items = new Object[100];
+    int putptr, takeptr, count;
+
+    // 生产
+    public void put(Object x) throws InterruptedException {
+        lock.lock();
+        try {
+            while (count == items.length)
+                notFull.await();  // 队列已满，等待，直到 not full 才能继续生产
+            items[putptr] = x;
+            if (++putptr == items.length) putptr = 0;
+            ++count;
+            notEmpty.signal(); // 生产成功，队列已经 not empty 了，发个通知出去
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    // 消费
+    public Object take() throws InterruptedException {
+        lock.lock();
+        try {
+            while (count == 0)
+                notEmpty.await(); // 队列为空，等待，直到队列 not empty，才能继续消费
+            Object x = items[takeptr];
+            if (++takeptr == items.length) takeptr = 0;
+            --count;
+            notFull.signal(); // 被我消费掉一个，队列 not full 了，发个通知出去
+            return x;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+
+}
---
+++ java-basic/src/main/java/me/lzb/java/basic/concurrent/LockDemo.java
@@ -0,0 +1,16 @@
+package me.lzb.java.basic.concurrent;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by lzb on 19/8/5
+ */
+public class LockDemo {
+
+
+    private static ReentrantLock reentrantLock = new ReentrantLock(false);
+
+
+
+
+}
