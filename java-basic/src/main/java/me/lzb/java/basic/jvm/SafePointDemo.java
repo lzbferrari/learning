a0044c9427a7b95c8910c582c42f3fa3d4ccdaae
egan <egan@2dfire.com>
2019-09-02 20:43:29 +0800

    提交

---
+++ java-basic/src/main/java/me/lzb/java/basic/jvm/SafePointDemo.java
@@ -0,0 +1,49 @@
+package me.lzb.java.basic.jvm;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 一个线程会不会出现这种情况，不在安全区域内，然后也一直执行不到安全点，比如死循环，然后辣鸡回收就开始不了了？
+ * jvm配置  -Xmx40m -XX:+PrintGCDetails
+ * 这种情况写不出来
+ * Created by egan on 19/9/2
+ */
+public class SafePointDemo {
+
+    public static final Map<String, Object> map = new HashMap<>();
+    public static char c = '1';
+    public static Object b = null;
+    public static void main(String[] args) throws IOException {
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Object a = new Object();
+                while (c == '1') {
+                    a = new Object();
+                }
+                b = a;
+            }
+        }, "loop");
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++) {
+                    map.put(String.valueOf(i), new Object());
+                }
+                map.put(String.valueOf(Math.random()), b);
+            }
+        }, "create");
+
+        t1.start();
+        t2.start();
+
+        char i = (char)System.in.read();
+        c = i;
+        System.out.println(c);
+    }
+
+}
