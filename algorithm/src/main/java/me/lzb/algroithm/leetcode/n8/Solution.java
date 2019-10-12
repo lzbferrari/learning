6cd6796bb3e1257250d2b1f96800957e797e3626
egan <egan@2dfire.com>
2019-10-12 15:51:20 +0800

    leetcode#8

--- algorithm/src/main/java/me/lzb/algroithm/leetcode/n8/Solution.java
+++ algorithm/src/main/java/me/lzb/algroithm/leetcode/n8/Solution.java
@@ -7,7 +7,12 @@ import java.util.regex.Pattern;
  * Created by egan on 19/10/11
  */
 class Solution {
-    public static int myAtoi(String str) {
+    /**
+     * 正则操作
+     * @param str
+     * @return
+     */
+    static int myAtoi(String str) {
 
         if (str == null) {
             return 0;
@@ -56,10 +61,105 @@ class Solution {
 
     }
 
+    /**
+     * 遍历找数字
+     * @param str
+     * @return
+     */
+    static int myAtoi2(String str) {
+        if (str == null || str.length() == 0) {
+            return 0;
+        }
+        final char space = ' ';;
+        final char plus = '+';
+        final char minus = '-';
+        final char zero = '0';
+        boolean isNegative = false;
+        char[] chars = str.toCharArray();
+        char[] tmpArray = new char[chars.length + 1];
+
+        int idx = 0;
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (idx == 0 && c == space) {
+                continue;
+            }
+            if (idx > 11) {
+                break;
+            }
+
+            if (idx == 0) {
+                if (minus == c) {
+                    isNegative = true;
+                    tmpArray[idx] = c;
+                    idx = idx + 1;
+                    continue;
+                }
+
+                if (plus == c) {
+                    tmpArray[idx] = c;
+                    idx = idx + 1;
+                    continue;
+                }
+
+                if (Character.isDigit(c)) {
+                    tmpArray[idx] = plus;
+                    idx = idx + 1;
+                    if (zero != c) {
+                        tmpArray[idx] = c;
+                        idx = idx + 1;
+                    }
+                    continue;
+                }
+                break;
+            }
+            if (idx == 1) {
+                if (zero == c) {
+                    continue;
+                }
+            }
+
+            if (!Character.isDigit(c)) {
+                break;
+            }
+
+            tmpArray[idx] = c;
+            idx = idx + 1;
+        }
+
+
+        if (idx <= 1) {
+            return 0;
+        }
+
+        char[] numbers = new char[idx];
+        System.arraycopy(tmpArray, 0, numbers, 0, idx);
+
+        if (isNegative) {
+            return (int) Math.max(Integer.MIN_VALUE, Long.parseLong(String.valueOf(numbers)));
+        } else {
+            return (int) Math.min(Integer.MAX_VALUE, Long.parseLong(String.valueOf(numbers)));
+        }
+    }
+
+    /**
+     * 遍历+直接计算
+     * @param str
+     * @return
+     */
+    static int myAtoi3(String str) {
+        return 0;
+    }
 
     public static void main(String[] args) {
 //        System.out.println(Integer.MIN_VALUE);
-        System.out.println(myAtoi("  0000000000012345678"));
+        System.out.println(myAtoi2("20000000000000000000"));
+
+//        System.out.println(String.valueOf(Integer.MAX_VALUE).length());
+//        System.out.println(String.valueOf(Integer.MIN_VALUE).length());
+//        System.out.println(Long.valueOf("99999999999"));
+//
+//        System.out.println("+".charAt(0));
     }
 
 
