c3e923328e53ad758dab9b8cc06ed691c63402d6
lzb <lzbferrari@gmail.com>
2019-10-23 01:31:52 +0800

    堆排序

--- algorithm/src/main/java/me/lzb/algroithm/sort/HeapSort.java
+++ algorithm/src/main/java/me/lzb/algroithm/sort/HeapSort.java
@@ -1,14 +1,100 @@
 package me.lzb.algroithm.sort;
 
+import java.util.Arrays;
+
 /**
  * 利用完全二叉树结构进行排序
  * 大顶堆：每个节点的值都大于或等于其子节点的值，在堆排序算法中用于升序排列
  * 小顶堆：每个节点的值都小于或等于其子节点的值，在堆排序算法中用于降序排列
+ * <p>
+ * 堆排序的时间复杂度O(N*logN),额外空间复杂度O(1)，是一个不稳定性的排序
+ * 参考：https://blog.csdn.net/u010452388/article/details/81283998
  *
- * 参考：https://www.jianshu.com/p/0d383d294a80
  * @author LZB
  */
 public class HeapSort {
+
+
+    public static void main(String[] args) {
+        int[] array = {2,3,1,5,7,9,10,8,6,4};
+        heapSort(array);
+        System.out.println(Arrays.toString(args));
+    }
+
+
+    //堆排序
+    private static void heapSort(int[] arr) {
+        //构造大根堆
+        heapInsert(arr);
+        int size = arr.length;
+        while (size > 1) {
+            //固定最大值
+            swap(arr, 0, size - 1);
+            size--;
+            //构造大根堆
+            heapify(arr, 0, size);
+
+        }
+
+    }
+
+    //构造大根堆（通过新插入的数上升）
+    private static void heapInsert(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            //当前插入的索引
+            int currentIndex = i;
+            //父结点索引
+            int fatherIndex = (currentIndex - 1) / 2;
+            //如果当前插入的值大于其父结点的值,则交换值，并且将索引指向父结点
+            //然后继续和上面的父结点值比较，直到不大于父结点，则退出循环
+            while (arr[currentIndex] > arr[fatherIndex]) {
+                //交换当前结点与父结点的值
+                swap(arr, currentIndex, fatherIndex);
+                //将当前索引指向父索引
+                currentIndex = fatherIndex;
+                //重新计算当前索引的父索引
+                fatherIndex = (currentIndex - 1) / 2;
+            }
+        }
+    }
+
+    //将剩余的数构造成大根堆（通过顶端的数下降）
+    private static void heapify(int[] arr, int index, int size) {
+        int left = 2 * index + 1;
+        int right = 2 * index + 2;
+        while (left < size) {
+            int largestIndex;
+            //判断孩子中较大的值的索引（要确保右孩子在size范围之内）
+            if (arr[left] < arr[right] && right < size) {
+                largestIndex = right;
+            } else {
+                largestIndex = left;
+            }
+            //比较父结点的值与孩子中较大的值，并确定最大值的索引
+            if (arr[index] > arr[largestIndex]) {
+                largestIndex = index;
+            }
+            //如果父结点索引是最大值的索引，那已经是大根堆了，则退出循环
+            if (index == largestIndex) {
+                break;
+            }
+            //父结点不是最大值，与孩子中较大的值交换
+            swap(arr, largestIndex, index);
+            //将索引指向孩子中较大的值的索引
+            index = largestIndex;
+            //重新计算交换之后的孩子的索引
+            left = 2 * index + 1;
+            right = 2 * index + 2;
+        }
+
+    }
+
+    //交换数组中两个元素的值
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 
 
 
